d5bfe9334be7c8098959bbf29194502b075715b1
Bebi00 <101123774+Bebi00@users.noreply.github.com>
2022-03-17 22:36:37 +0200

    W2D4

--- Week2/JSPTest/src/main/java/Login.java
+++ Week2/JSPTest/src/main/java/Login.java
@@ -1,10 +1,15 @@
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
-import java.net.http.HttpClient;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet(name="login",urlPatterns = "/login")
 
 public class Login extends HttpServlet {
-
-
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("src/main/VisitorCount.jsp").forward(req,resp);
+    }
 }
